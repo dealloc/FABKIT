f5020dccd9327adb5911971da198ca3049cc7f9a
Wannes Gennar <wannes.gennar+github@gmail.com>
2026-01-25 19:48:08 +0100

    feat: setup router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,26 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { ThemeProvider } from "next-themes";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
+
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen";
+
+// Create a new router instance
+const router = createRouter({ routeTree });
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+	interface Register {
+		router: typeof router;
+	}
+}
 
 const root = document.getElementById("root");
 createRoot(root || document.body).render(
 	<StrictMode>
-		<App />
+		<ThemeProvider>
+			<RouterProvider router={router} />
+		</ThemeProvider>
 	</StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,39 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+		}),
 		react({
 			babel: {
 				plugins: [["babel-plugin-react-compiler"]],
+			},
+		}),
+		VitePWA({
+			includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+			manifest: {
+				name: "FabKit",
+				short_name: "FabKit",
+				description: "Your Flesh and Blood Toolbox",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+				],
 			},
 		}),
 	],
