543bcd1b40881907a349fcebe8d9e37e730efcb4
Wannes Gennar <wannes.gennar@gmail.com>
2026-01-26 21:15:22 +0100

    feat: add card rarity types

--- src/config/cards.ts
+++ src/config/cards.ts
@@ -1,4 +1,5 @@
 // Contains configuration data for FaB cards and related types as well as utility types.
 
 export type { CardFormField } from "./cards/form_fields";
+export { CardRarities, type CardRarity } from './cards/rarities';
 export { type CardType, CardTypes } from "./cards/types";
---
+++ src/config/cards/rarities.ts
@@ -0,0 +1,14 @@
+export const CardRarities = {
+    basic: '/img/rarities/rarity_basic.svg',
+    common: '/img/rarities/rarity_common.svg',
+    fabled: '/img/rarities/rarity_fabled.svg',
+    legendary: '/img/rarities/rarity_legendary.svg',
+    majestic: '/img/rarities/rarity_majestic.svg',
+    marvel: '/img/rarities/rarity_marvel.svg',
+    promo: '/img/rarities/rarity_promo.svg',
+    rare: '/rarities/rarity_rare.svg',
+    superrare: '/rarities/rarity_superrare.svg',
+    token: '/img/rarities/rarity_token.svg',
+};
+
+export type CardRarity = keyof typeof CardRarities;
