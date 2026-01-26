9d61509200e98dc8d73a2e7ca78182e44e7f192b
Wannes Gennar <wannes.gennar+github@gmail.com>
2026-01-26 17:53:26 +0100

    feat: add 404 page

--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,13 @@ import "./i18n.ts";
 
 // Import the generated route tree
 import { routeTree } from "./routeTree.gen";
+import {NotFound} from "./routes/not-found.tsx";
 
 // Create a new router instance
-const router = createRouter({ routeTree });
+const router = createRouter({
+	routeTree,
+	defaultNotFoundComponent: () => <NotFound />
+});
 
 // Register the router instance for type safety
 declare module "@tanstack/react-router" {
